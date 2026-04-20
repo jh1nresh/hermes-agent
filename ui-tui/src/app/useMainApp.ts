9cc768ee12282e9ebc80c4aa7cdf72a1db5f0682
Brooklyn Nicholson <brooklyn.bb.nicholson@gmail.com>
2026-04-20 13:02:32 -0500

    chore: uptick

--- ui-tui/src/app/useMainApp.ts
+++ ui-tui/src/app/useMainApp.ts
@@ -1,4 +1,4 @@
-import { type ScrollBoxHandle, useApp, useHasSelection, useSelection, useStdout, useTerminalTitle } from '@hermes/ink'
+import { useApp, useHasSelection, useSelection, useStdout, useTerminalTitle, type ScrollBoxHandle } from '@hermes/ink'
 import { useStore } from '@nanostores/react'
 import { useCallback, useEffect, useMemo, useRef, useState } from 'react'
 
