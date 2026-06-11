9b06827f869dc0ac679a8dcdbca8d02916beae99
ethernet <arilotter@gmail.com>
2026-06-11 16:40:37 -0400

    change(tooling): update node to 26 everywhere, keep node version managed

--- nix/lib.nix
+++ nix/lib.nix
@@ -21,7 +21,7 @@ let
 
   # Single npm deps fetch from the workspace root lockfile.
   # All workspace packages share this derivation.
-  npmDepsHash = "sha256-BfTSh6J2VZ/07tq2DYnKgUViZCgRhW1sC2uj18H65SE=";
+  npmDepsHash = "sha256-+MqqcCsnUSMbsAooGKRx+qD/r/kPZG0Y8rfHuFGlHSU=";
 
   npmDeps = pkgs.fetchNpmDeps {
     inherit src;
