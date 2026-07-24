f74dd4cea647792d5bd062abbf39b13014a9aef3
ethernet <arilotter@gmail.com>
2026-07-24 18:58:09 -0400

    change(desktop): don't show commit hash on toolbar
    also clean up tooltip

--- apps/desktop/src/app/shell/hooks/use-statusbar-items.tsx
+++ apps/desktop/src/app/shell/hooks/use-statusbar-items.tsx
@@ -225,11 +225,10 @@ export function useStatusbarItems({
       branch && copy.branch(branch)
     ]
       .filter(Boolean)
-      .join(' · ')
+      .join('\n')
 
     return {
       className: !applying && behind > 0 ? 'text-primary hover:text-primary' : undefined,
-      detail: appVersion && sha && !applying && !remote ? sha : undefined,
       hidden: !appVersion && !sha,
       icon: applying ? <Loader2 className="size-3 animate-spin" /> : <Hash className="size-3" />,
       id: 'version-client',
@@ -278,7 +277,7 @@ export function useStatusbarItems({
       backendVersion && copy.backendVersion(backendVersion)
     ]
       .filter(Boolean)
-      .join(' · ')
+      .join('\n')
 
     return {
       className: !applying && updateAvailable ? 'text-primary hover:text-primary' : undefined,
